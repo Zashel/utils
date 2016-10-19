7bb7585bb5c9467d302e6089a7db6b4224ff6d3c
Iván Uría <ivan.uria@gmail.com>
2016-10-19 11:40:31 +0200

    Create win.py

---
+++ utils/win.py
@@ -0,0 +1,24 @@
+import os
+from .exceptions import PathError
+
+#Not sure I wrote this code. 
+#I suppose I didn't, but found it as tutorials.
+#If this is yours, please, tell me.
+
+def search_win_drive(path):
+    '''
+    Searches for a drive in a windows machine given a path to 
+    look for from root.
+    Returns the full path with the given path or raises
+    PathError if not found.
+    '''
+    drives = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    for drive in drives:
+        full_path = r"{}:\{}".format(drive, path)
+        if os.path.exists(full_path):
+            return full_path
+    else:
+        raise PathError 
+
+#For compatibilities with other modules and stuff of my own:
+buscar_unidad = search_win_drive
