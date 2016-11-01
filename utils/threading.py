ba7f5bde564671c55f909a602b46fbb24e3d9a9a
Iván Uría <ivan.uria@gmail.com>
2016-11-01 19:39:29 +0100

    Fixed Bugs

--- utils/threading.py
+++ utils/threading.py
@@ -14,9 +14,9 @@ def threadize(function):
                 function(*args, **kwargs)
         t = ThreadDeco()
         t.start()
+        t.__name__ = function.__name__
         return t
     return inner
-    _threadize.__name__ = function.__name__
 
 def daemonize(function):
     '''
@@ -24,15 +24,15 @@ def daemonize(function):
     as a hell's Daemon.
     '''
     def inner(*args, **kwargs):
-        class ThreadDeco(Thread):
+        class DaemonDeco(Thread):
             def run(self):
                 function(*args, **kwargs)
-        t = ThreadDeco()
+        t = DaemonDeco()
         t.setDaemon(True)
         t.start()
+        t.__name__ = function.__name__
         return t
     return inner
-    _daemonize.__name__ = function.__name__
     
 #For compatibilities with other modules and stuff of my own:
 make_thread = threadize
